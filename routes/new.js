6d74ebdecc905f09347cd8b6e2122830ebfb96c3
hipip <bt.youcef.dz@gmail.com>
2025-02-14 17:08:38 +0100

    Add new message post feature

--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
+const messages = require("../db");
 
 const router = Router();
 
 router.get("/", (req, res) => {
   res.render("new", { title: "Add Message Page" });
+});
+
+router.post("/", (req, res) => {
+  const { user, text } = req.body;
+  messages.push({ user, text, date: new Date() });
+  res.redirect("//new");
 });
 
 module.exports = router;
